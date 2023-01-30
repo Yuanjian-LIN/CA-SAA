4e915df7650ff24abf0bf68f589af66e9e8b6c22
Yuanjian-LIN <122502370+Yuanjian-LIN@users.noreply.github.com>
2023-01-30 12:16:22 +0800

    3D reconstruction program

--- A-300.py
+++ A-300.py
@@ -291,6 +291,6 @@ if __name__ == '__main__':
     sample_time = 10000
     # Parallel computation parameter
     process_number = 10
-    # 
+    # 1
     size_gap = 1
     main_program(target_pic, pic_number, pic_row,  pic_column, change_time, critical_zone_number, sample_time, process_number, size_gap)
